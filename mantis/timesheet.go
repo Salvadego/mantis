8e7a85fb72dff5231fbcac16ec071627edbffa4f
Salvadego <salvadegodev@gmail.com>
2025-09-17 15:59:36 -0300

    feature: added get method in timesheet service

--- mantis/timesheet.go
+++ mantis/timesheet.go
@@ -92,6 +92,40 @@ func (s *TimesheetService) GetTimesheets(
 	return result.Value, nil
 }
 
+
+func (s *TimesheetService) Get(
+	ctx context.Context,
+	timesheetId int) ([]TimesheetsResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	endpoint := "api/odata/cam/core/system/v1/MTS_Timesheets"
+	filter := fmt.Sprintf(
+		"$filter=timesheetId eq %d",
+		timesheetId)
+
+	path := fmt.Sprintf("%s?%s", endpoint, url.PathEscape(filter))
+
+	headers := map[string]string{
+		"SourceSystem": "APP",
+	}
+
+	resp, err := s.client.doRequest(ctx, http.MethodGet, path, nil, headers)
+	if err != nil {
+		return nil, err
+	}
+
+	var result struct {
+		Value []TimesheetsResponse `json:"value"`
+	}
+
+	if err := parseResponse(resp, &result); err != nil {
+		return nil, err
+	}
+
+	return result.Value, nil
+}
+
 func (s *TimesheetService) GetProjectTimesheets(
 	ctx context.Context,
 	employeeId int) ([]ProjectTimesheet, error) {
